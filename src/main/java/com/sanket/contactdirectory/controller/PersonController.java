d833c61dfd19dcbbd6cde602ba72740d57a13526
SanketRathod007 <92321563+SanketRathod007@users.noreply.github.com>
2023-03-31 00:28:29 +0530

    31st march commit

--- src/main/java/com/sanket/contactdirectory/controller/PersonController.java
+++ src/main/java/com/sanket/contactdirectory/controller/PersonController.java
@@ -1,5 +1,70 @@
 package com.sanket.contactdirectory.controller;
 
-public class PersonController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sanket.contactdirectory.entity.Person;
+import com.sanket.contactdirectory.service.PersonService;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+    
+    private PersonService personService;
+    
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+    
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.getAllPersons();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable long id) {
+        try {
+            Person person = personService.getPersonById(id);
+            return ResponseEntity.ok(person);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping
+    public Person addPerson(@RequestBody Person person) {
+        return personService.addPerson(person);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable long id, @RequestBody Person person) {
+        try {
+            Person updatedPerson = personService.updatePerson(id, person);
+            return ResponseEntity.ok(updatedPerson);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePerson(@PathVariable long id) {
+        String result = personService.deletePerson(id);
+        if (result.equals("Person is deleted")) {
+            return ResponseEntity.ok(result);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
 }
